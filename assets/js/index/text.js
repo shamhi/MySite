8d84a36ff1e1a483d4081967c1ca93c7a47c408c
shamhi <shomakakdela@gmail.com>
2024-03-10 10:53:24 +0300

    Initial

---
+++ assets/js/index/text.js
@@ -0,0 +1,30 @@
+$(function (){
+    let title = document.title
+    let animSeq = ["|"]
+    let animIndex = 0
+    let titleIndex = 0;
+
+    function doInverseSpinZeroPitch() {
+        let loadTitle = title.substring(0, titleIndex);
+        if (titleIndex > title.length) {
+            animIndex = 0;
+            titleIndex = 0;
+        }
+        if (animIndex > 1) {
+            titleIndex++;
+            animIndex = 0;
+        }
+        document.title = loadTitle + animSeq[0];
+        animIndex++;
+    }
+    window.setInterval(doInverseSpinZeroPitch, 150);
+});
+
+$(document).ready(function () {
+    $('.typed').typed({
+        strings: ["17 years old", "Fullstack Developer", "Action Equals Reaction", "Anti-Hacker", "Nukkutuk", "Mamluk", "Off yaaaa"],
+        typeSpeed: 100,
+        loop: true,
+        cursorChat: "|",
+    });
+});
